0983e7a06f7bdbd86cc4eb1900e396746ea99545
Salah Mohamed <salahmohamedabdelhalim@gmail.com>
2023-09-06 19:01:58 +0300

    clone pro

--- program.cs
+++ program.cs
@@ -1,1 +1,106 @@
-string sààs = "ààà";
+using System;
+
+namespace eSourcing.Enums
+{
+    /// <summary>
+    ///
+    /// </summary>
+    [Serializable]
+    public enum ETipoAssociazione : Int32
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        ImpresaSocieta,
+
+        /// <summary>
+        ///
+        /// </summary>
+        LiberoProfessionista,
+
+        /// <summary>
+        ///
+        /// </summary>
+        AssociazioneDiLiberiProfessionisti,
+
+        /// <summary>
+        ///
+        /// </summary>
+        Societ�DiProfessionisti,
+
+        /// <summary>
+        ///
+        /// </summary>
+        Societ�DiIngegneria,
+
+        /// <summary>
+        ///
+        /// </summary>
+        PrestatoriDiServiziDiIngegneriaEArchitettura,
+
+        /// <summary>
+        ///
+        /// </summary>
+        Altro,
+
+        /// <summary>
+        ///
+        /// </summary>
+        ConsorzioFraCooperative,
+
+        /// <summary>
+        ///
+        /// </summary>
+        ConsorzioStabile,
+
+        /// <summary>
+        ///
+        /// </summary>
+        RTICostituendo,
+
+        /// <summary>
+        ///
+        /// </summary>
+        ConsorzioOrdinarioDiConcorrentiCostituendo,
+
+        /// <summary>
+        ///
+        /// </summary>
+        GEIECostituendo,
+
+        /// <summary>
+        ///
+        /// </summary>
+        RTICostituito,
+
+        /// <summary>
+        ///
+        /// </summary>
+        ConsorzioOrdinarioDiConcorrentiCostituito,
+
+        /// <summary>
+        ///
+        /// </summary>
+        GEIECostituito,
+
+        /// <summary>
+        ///
+        /// </summary>
+        ReteOrganoComunePotereRapprSoggGiuridicaCostituito,
+
+        /// <summary>
+        ///
+        /// </summary>
+        ReteOrganoComunePotereRapprNoSoggGiuridicaCostituendo,
+
+        /// <summary>
+        ///
+        /// </summary>
+        ReteOrganoComuneNoPotereRapprNoRequisitiCostituito,
+
+        /// <summary>
+        ///
+        /// </summary>
+        ReteOrganoComuneNoPotereRapprNoRequisitiCostituendo,
+    }
+}
